e8a7650cffced00efe574f9f39fd55595d8a0619
DenisseBautista <99107850+DenisseBautista@users.noreply.github.com>
2023-09-02 09:49:59 -0600

    Practica1_Firma
    Se elimino el tache de cerrar y se agrego la función close.

---
+++ Proyecto1/Proyecto1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Proyecto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.BackColor = System.Drawing.Color.Pink;
+            this.Label1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(33, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(365, 85);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Nombre:Denisse Bautista Garcia.\r\nBoleta:2020300078.\r\nEscuela:Escuela Superior De " +
+    "Ingeniería Mécanica y Eléctrica.\r\nGrupo:8CM12.\r\nMateria:Ingeniería De Software.\r" +
+    "\n";
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("PictureBox1.ErrorImage")));
+            this.PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox1.Image")));
+            this.PictureBox1.Location = new System.Drawing.Point(118, 109);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(160, 154);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 2;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(416, 290);
+            this.ControlBox = false;
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Label1);
+            this.DoubleBuffered = true;
+            this.IsMdiContainer = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.PictureBox PictureBox1;
+    }
+}
+
